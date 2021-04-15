fa7732fca7d0898b259822e640d289107110341c
marcanta <aime.marcant@hotmail.fr>
2021-04-15 09:39:46 +0200

    feat: add documentation route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import couponRoutes from './routes/coupon';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import apiDoc from "./assets/openapi.json";
+import swaggerUi from "swagger-ui-express";
+import couponRoutes from "./routes/coupon";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,33 +13,35 @@ const dbname = process.env.MONGODB_DB;
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.mff6b.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 
-console.log(uri); 
-mongoose.connect(uri,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+console.log(uri);
+mongoose
+	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-  
-  app.use(bodyParser.json());
-  
-  app.use("/coupon", couponRoutes);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+	next();
+});
 
-  //default route "/"
-  app.get("/", (req, res) => {
-    res.status(200).json({ Welcome: "Welcome to the MSPR API" });
-  });
+app.use(bodyParser.json());
 
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(apiDoc));
 
-  const listener = app.listen(port, () => {
-    console.log(`Server is running on port ${listener.address().port}.`);
-  });
+app.use("/coupon", couponRoutes);
+
+//default route "/"
+app.get("/", (req, res) => {
+	res.status(200).json({ Welcome: "Welcome to the MSPR API" });
+});
+
+const listener = app.listen(port, () => {
+	console.log(`Server is running on port ${listener.address().port}.`);
+});
 
 export default app;
